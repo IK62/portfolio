ad33c400fb741dc1f4409ad66c668bd179ed41f4
IK62 <ismail.karrouch13@gmail.com>
2024-03-15 13:27:36 +0000

    yet another fix

--- src/routers.jsx
+++ src/routers.jsx
@@ -6,7 +6,7 @@ import About from './pages/about'
 
 function Routers() {
   return (
-    <Router root={Layout}>
+    <Router base='/portfolio' root={Layout}>
       <Route path="/" component={Home}/>
       <Route path="/works/:name?" component={Works}/>
       <Route path="/work/:name?" component={Works}/>
