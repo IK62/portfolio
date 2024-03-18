334c5f45ff74b88595e030283f818459afe9e6dd
IK62 <ismail.karrouch13@gmail.com>
2024-03-18 14:00:17 +0000

    modified work page

--- src/pages/works.jsx
+++ src/pages/works.jsx
@@ -52,9 +52,11 @@ function Works() {
     <>
       {currentWork() ? (
         <div>
-          <div class='flex justify-between p-10 pb-5 items-end'>
-            <h2 class='text-4xl font-normal'>{currentWork().title}</h2>
-            <div class='h-28'>
+          <div class="flex justify-between flex-wrap py-2 px-[2%] pb-2 items-end [&>*]:mx-auto gap-2">
+            <a href={currentWork().link} target='_blank'>
+              <h2 class="text-base font-normal underline sm:text-xl xl:text-2xl 2xl:text-3xl">{currentWork().title}</h2>
+            </a>
+            <div class="h-10 sm:h-16 xl:h-20 2xl:h-24">
               <img
                 src={
                   new URL(
@@ -63,14 +65,16 @@ function Works() {
                   ).href
                 }
                 alt=""
-                class='h-full'
+                class="h-full w-full object-cover"
               />
             </div>
           </div>
-          <div class='sectionDivider'></div>
-          <div class='mt-5'>
-            <p class='text-sm'>
-              <span class='float-right mb-4 ml-10 text-base'>{currentWork().type}</span>
+          <div class="sectionDivider"></div>
+          <div class="mt-1">
+            <p class="text-[0.25rem] min-[450px]:text-[0.4rem] sm:text-[0.6rem] lg:text-xs xl:text-sm 2xl:text-base">
+              <span class="float-right mb-1 ml-1 text-[0.5rem] min-[450px]:text-[0.6rem] sm:text-xs lg:text-base sm:ml-2 sm:mb-0 lg:ml-3 lg:mb-2 2xl:text-xl 2xl:ml-5 2xl:mb-3">
+                {currentWork().type}
+              </span>
               {currentWork().description}
             </p>
           </div>
@@ -95,7 +99,9 @@ function Works() {
               alt=""
               class="object-cover w-full h-full blur-[3px]"
             />
-            <h2 class="absolute text-xs lg:text-2xl 2xl:text-5xl 2xl:font-normal">{currentText()}</h2>
+            <h2 class="absolute text-xs lg:text-2xl 2xl:text-5xl 2xl:font-normal">
+              {currentText()}
+            </h2>
           </div>
           <CarouselButtons
             toRight={true}
